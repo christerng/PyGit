4f2b78d44caaea8b143b34a27c5e5c50cfe702eb
Christopher Terence Ng <christerng@gmail.com>
2021-10-09 23:15:41 +0800

    feat: add argument parser

--- pygit/cli.py
+++ pygit/cli.py
@@ -1,2 +1,22 @@
-def main():
+import argparse
+from argparse import ArgumentParser, Namespace
+
+
+def main() -> None:
+    args = parse_args()
+    args.func(args)
+
+
+def parse_args() -> Namespace:
+    parser = ArgumentParser()
+
+    commands = parser.add_subparsers(dest="command", required=True)
+
+    init_parser = commands.add_parser("init")
+    init_parser.set_defaults(func=init)
+
+    return parser.parse_args()
+
+
+def init(args:  Namespace) -> None:
     print("Hello, World")
