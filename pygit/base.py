a3372885cf41dda1533e786b89f3cdffafbcbf49
Christopher Terence Ng <christerng@gmail.com>
2021-10-12 20:56:40 +0800

    feat: hash files

--- pygit/base.py
+++ pygit/base.py
@@ -4,15 +4,16 @@ from . import base
 from . import data
 
 
+def is_ignored(path: Path) -> bool:
+    return str(data.GIT_DIR) in str(path)
+
+
 def write_tree(directory: Path = Path()) -> None:
     for child in directory.iterdir():
         if is_ignored(child):
             continue
         if child.is_file() and not child.is_symlink():
-            print(child)
+            with open(child, "rb") as f:
+                print(data.hash_object(f.read()), child)
         elif child.is_dir() and not child.is_symlink():
             write_tree(child)
-
-
-def is_ignored(path: Path) -> bool:
-    return str(data.GIT_DIR) in str(path)
